1e2783c40ccf0ad2c6e2182ffa103a2ae9d2173f
bob <don@improving.com>
2022-08-03 14:54:51 -0400

    Added RegisterMember functionality

--- src/test/java/com/improving/stepdefs/StepDefinition.java
+++ src/test/java/com/improving/stepdefs/StepDefinition.java
@@ -1,18 +1,24 @@
 package com.improving.stepdefs;
 
+import com.improving.tddair.Member;
+import com.improving.tddair.TddAirApplication;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class StepDefinition {
+
+    private TddAirApplication app;
+
     @When("member registers with username {string} and email {string}")
-    public void member_registers_with_username_and_email(String string, String string2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void member_registers_with_username_and_email(String username, String email) {
+        app = new TddAirApplication();
+        app.registerMember(username, email);
     }
     @Then("member account exists with username {string}")
-    public void member_account_exists_with_username(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void member_account_exists_with_username(String username) {
+        Member member = app.lookupMember(username);
+        Assertions.assertEquals(username, member.getUsername());
     }
     @Then("member has status of {string}")
     public void member_has_status_of(String string) {
